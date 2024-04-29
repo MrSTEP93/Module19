28df77dcbf94890f9caa6bb9c2ca3c2a35572543
Shatalov.AG <mrstep93@ya.ru>
2024-04-30 00:24:30 +0300

    Добавьте файлы проекта.

---
+++ Module19.Final/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Module19.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome from MrSTEP's unsocial club!");
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                string userName = Console.ReadLine();
+                if (ValidateName(userName)) 
+                    break;
+                Console.WriteLine("Username must contains a correct value!!!");
+
+            }
+            Console.Write("Type your password: ");
+
+        }
+
+        private static bool ValidateName(string userName)
+        {
+            if (string.IsNullOrEmpty(userName))
+                return false;
+            else
+                return true;
+        }
+    }
+}
