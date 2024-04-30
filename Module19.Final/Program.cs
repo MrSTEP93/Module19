9dcfdcda3e9fdfa06a8ed1a43da7e01bad4618e4
Shatalov.AG <mrstep93@ya.ru>
2024-05-01 00:40:33 +0300

    накидали готовых классов, красавчики

--- Module19.Final/Program.cs
+++ Module19.Final/Program.cs
@@ -1,23 +1,51 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Module19.Final
 {
     internal class Program
     {
+
+        enum DataType
+        {
+            surname,
+            name,
+            password,
+            email
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome from MrSTEP's unsocial club!");
+            AskUserData(DataType.surname);
+            AskUserData(DataType.name);
+            AskUserData(DataType.password);
+            AskUserData(DataType.email);
+            
+        }
+
+        private static void AskUserData(DataType dataType)
+        {
             while (true)
             {
-                Console.Write("Type your name: ");
-                string userName = Console.ReadLine();
-                if (ValidateName(userName)) 
-                    break;
-                Console.WriteLine("Username must contains a correct value!!!");
-
+                Console.Write($"Type your {dataType}: ");
+                string userInput = Console.ReadLine();
+                if ((dataType == DataType.name) || (dataType == DataType.surname))
+                    if (ValidateName(userInput))
+                        break;
+                    else
+                        Console.WriteLine($"{dataType} must contains a correct value!!!");
+                if (dataType == DataType.password)
+                    if (ValidatePass(userInput))
+                        break;
+                    else
+                        Console.WriteLine("Password must be longer than 7 symbols");
+                if (dataType == DataType.email) 
+                    if (ValidateEmail(userInput))
+                        break;
+                    else
+                        Console.WriteLine("Please, type correct email");
             }
-            Console.Write("Type your password: ");
-
         }
 
         private static bool ValidateName(string userName)
@@ -26,6 +54,22 @@ namespace Module19.Final
                 return false;
             else
                 return true;
+        }
+
+        private static bool ValidatePass(string password)
+        {
+            if (password.Length < 8)
+                return false;
+            else
+                return true;
+        }
+
+        private static bool ValidateEmail(string email)
+        {
+            if (new EmailAddressAttribute().IsValid(email))
+                return true;
+            else
+                return false;
         }
     }
 }
