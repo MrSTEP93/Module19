0b3543ff10d88d4940ffaac47de159d53c9fb16e
Shatalov.AG <mrstep93@ya.ru>
2024-05-01 23:50:27 +0300

    добавлена работа с инфо пользователя

---
+++ Module19.Final/BLL/Services/UserService.cs
@@ -0,0 +1,102 @@
+﻿using Module19.Final.BLL.Exceptions;
+using Module19.Final.BLL.Models;
+using Module19.Final.DAL.Entities;
+using Module19.Final.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Module19.Final.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public void Register(UserRegistrationData userRegData)
+        {
+            Type resultType = userRegData.GetType();
+            foreach (var prop in resultType.GetProperties())
+            {
+                if (string.IsNullOrEmpty(prop.GetValue(userRegData, null).ToString()))
+                    throw new ArgumentNullException(prop.Name, "Parameter must contains non-empty value!");
+            }
+            
+            if (userRegData.Password.Length < 8)
+                throw new ArgumentNullException("Password", "Password must be longer than 7 characters");
+
+            if (!new EmailAddressAttribute().IsValid(userRegData.Email))
+                throw new ArgumentNullException("Email", "Email address must contains a correct value!");
+
+            if (userRepository.FindByEmail(userRegData.Email) != null)
+                throw new ArgumentException($"User with this email already registered!");
+
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegData.FirstName,
+                lastname = userRegData.LastName,
+                email = userRegData.Email,
+                password = userRegData.Password
+            };
+
+            if (userRepository.Create(userEntity) == 0)
+                throw new Exception("There are some errors while creating user. No details");
+        }
+
+        public User Authenticate(UserAuthenticationData userAuthenticationData)
+        {
+            var findUserEntity = userRepository.FindByEmail(userAuthenticationData.Email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            if (findUserEntity.password != userAuthenticationData.Password)
+                throw new WrongPasswordException();
+
+            return ConstructUserModel(findUserEntity);
+        }
+
+        public User FindByEmail(string email)
+        {
+            var findUserEntity = userRepository.FindByEmail(email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            return ConstructUserModel(findUserEntity);
+        }
+
+        public void Update(User user)
+        {
+            var updatableUserEntity = new UserEntity()
+            {
+                id = user.Id,
+                firstname = user.FirstName,
+                lastname = user.LastName,
+                password = user.Password,
+                email = user.Email,
+                photo = user.Photo,
+                favorite_movie = user.FavoriteMovie,
+                favorite_book = user.FavoriteBook
+            };
+
+            if (this.userRepository.Update(updatableUserEntity) == 0)
+                throw new Exception();
+        }
+
+        private User ConstructUserModel(UserEntity userEntity)
+        {
+            return new User(userEntity.id,
+                          userEntity.firstname,
+                          userEntity.lastname,
+                          userEntity.password,
+                          userEntity.email,
+                          userEntity.photo,
+                          userEntity.favorite_movie,
+                          userEntity.favorite_book);
+        }
+    }
+}
--- Module19.Final/Program.cs
+++ Module19.Final/Program.cs
@@ -1,75 +1,159 @@
-﻿using System;
+﻿using Module19.Final.BLL.Exceptions;
+using Module19.Final.BLL.Models;
+using Module19.Final.BLL.Services;
+using System;
 using System.ComponentModel.DataAnnotations;
+using static ConsoleHelper_50.Helper_50;
 
 namespace Module19.Final
 {
     internal class Program
     {
-
-        enum DataType
-        {
-            surname,
-            name,
-            password,
-            email
-        }
+        public static UserService userService = new UserService();
+        public static User user;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome from MrSTEP's unsocial club!");
-            AskUserData(DataType.surname);
-            AskUserData(DataType.name);
-            AskUserData(DataType.password);
-            AskUserData(DataType.email);
-            
-        }
+            WriteLnBg("Welcome from MrSTEP's unsocial club!", ConsoleColor.Black, ConsoleColor.Cyan);
 
-        private static void AskUserData(DataType dataType)
-        {
             while (true)
             {
-                Console.Write($"Type your {dataType}: ");
-                string userInput = Console.ReadLine();
-                if ((dataType == DataType.name) || (dataType == DataType.surname))
-                    if (ValidateName(userInput))
-                        break;
-                    else
-                        Console.WriteLine($"{dataType} must contains a correct value!!!");
-                if (dataType == DataType.password)
-                    if (ValidatePass(userInput))
-                        break;
-                    else
-                        Console.WriteLine("Password must be longer than 7 symbols");
-                if (dataType == DataType.email) 
-                    if (ValidateEmail(userInput))
-                        break;
-                    else
-                        Console.WriteLine("Please, type correct email");
+                WriteLnBg("Введите команду: ", ConsoleColor.Black, ConsoleColor.DarkCyan);
+                WriteLn("_Войти в профиль (нажмите 1)");
+                WriteLn("_Зарегистрироваться (нажмите 2)", true);
+
+                switch (ReadLn())
+                {
+                    case "1":
+                        {
+                            if (StartAuthentication())
+                                ShowMenu();
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            StartRegistration();
+                            break;
+                        }
+                }
             }
         }
 
-        private static bool ValidateName(string userName)
+
+        public static void WriteLnBg(string message, ConsoleColor textColor, ConsoleColor backgroundColor)
         {
-            if (string.IsNullOrEmpty(userName))
-                return false;
-            else
-                return true;
+            Console.BackgroundColor = backgroundColor;
+            WriteLn(message, textColor);
+            Console.BackgroundColor = ConsoleColor.Black;
         }
 
-        private static bool ValidatePass(string password)
+        static bool StartAuthentication()
         {
-            if (password.Length < 8)
-                return false;
-            else
+            var authenticationData = new UserAuthenticationData();
+
+            WriteLn("Введите почтовый адрес:", true);
+            authenticationData.Email = Console.ReadLine();
+
+            WriteLn("Введите пароль:", true);
+            authenticationData.Password = Console.ReadLine();
+
+            try
+            {
+                user = userService.Authenticate(authenticationData);
                 return true;
+            }
+            catch (WrongPasswordException)
+            {
+                WriteLn("Неверный пароль!", ConsoleColor.Red);
+                return false;
+            }
+            catch (UserNotFoundException)
+            {
+                WriteLn("Пользователь не найден!", ConsoleColor.Red);
+                return false;
+            }
         }
 
-        private static bool ValidateEmail(string email)
+        static void StartRegistration()
         {
-            if (new EmailAddressAttribute().IsValid(email))
-                return true;
-            else
-                return false;
+            var userRegData = new UserRegistrationData();
+
+            WriteLn("You need to be registered now");
+            Write($"\t Type your First Name: ", ConsoleColor.Blue);
+            userRegData.FirstName = ReadLn();
+            Write($"\t Type your Last Name: ", ConsoleColor.Blue);
+            userRegData.LastName = ReadLn();
+            Write($"\t Type your EMail: ", ConsoleColor.Blue);
+            userRegData.Email = ReadLn();
+            Write($"\t Type your Password (7+ symbols): ", ConsoleColor.Blue);
+            userRegData.Password = ReadLn();
+
+            try
+            {
+                userService.Register(userRegData);
+                WriteLn("Registration completed. Nice to see you!", ConsoleColor.Green);
+            }
+            catch (ArgumentNullException ex)
+            {
+                WriteLn("Invalid registration data: " + ex.Message, ConsoleColor.Red);
+            }
+            catch (ArgumentException ex)
+            {
+                WriteLn("Registration failed: " + ex.Message, ConsoleColor.DarkCyan);
+            }
+            catch (Exception exception)
+            {
+                WriteLn("Registration failed: " + exception.Message, ConsoleColor.DarkCyan);
+            }
+        }
+
+        static void ShowMenu()
+        {
+            WriteLn($"Вы успешно вошли в социальную сеть! Добро пожаловать {user.FirstName}", ConsoleColor.Green);
+            while (true)
+            {
+                WriteLnBg("Выберите действие: ", ConsoleColor.Black, ConsoleColor.DarkCyan);
+                WriteLn("_Просмотреть информацию о моём профиле (нажмите 1)");
+                WriteLn("_Редактировать мой профиль (нажмите 2)");
+                WriteLn("_Добавить в друзья (нажмите 3)");
+                WriteLn("_Написать сообщение (нажмите 4)");
+                WriteLn("_Выйти из профиля (нажмите 5)");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            WriteLn($"Информация о моем профиле");
+                            WriteLn($"Мой идентификатор: {user.Id}");
+                            WriteLn($"Меня зовут: {user.FirstName}");
+                            WriteLn($"Моя фамилия: {user.LastName}");
+                            WriteLn($"Мой пароль: {user.Password}");
+                            WriteLn($"Мой почтовый адрес: {user.Email}");
+                            WriteLn($"Ссылка на моё фото: {user.Photo}");
+                            WriteLn($"Мой любимый фильм: {user.FavoriteMovie}");
+                            WriteLn($"Моя любимая книга: {user.FavoriteBook}");
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.Write("Меня зовут:");
+                            user.FirstName = Console.ReadLine();
+                            Console.Write("Моя фамилия:");
+                            user.LastName = Console.ReadLine();
+                            Console.Write("Ссылка на моё фото:");
+                            user.Photo = Console.ReadLine();
+                            Console.Write("Мой любимый фильм:");
+                            user.FavoriteMovie = Console.ReadLine();
+                            Console.Write("Моя любимая книга:");
+                            user.FavoriteBook = Console.ReadLine();
+
+                            userService.Update(user);
+                            WriteLn("Ваш профиль успешно обновлён!", ConsoleColor.Green);
+                            break;
+                        }
+                }
+            }
         }
     }
 }
